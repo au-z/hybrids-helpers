7dafb2df0825daaef2cb0b760d2d6111cdcaf7c6
Austin Martin <auz.martin@gmail.com>
2024-04-30 16:22:12 -0400

    Release 0.6.2
    Patch:
    - restore missing reflect factory

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,12 @@ export * from './context.js'
 
 export * from './factories/connect.js'
 export * from './factories/disposable.js'
+export * from './factories/effect.js'
 export * from './factories/getset.js'
 export * from './factories/listen.js'
+export * from './factories/reflect.js'
 export * from './factories/ro.js'
 export * from './factories/set.js'
-export * from './factories/effect.js'
 
 export * from './refs/ref.js'
 
