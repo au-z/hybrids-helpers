695787c4060e040dde4ebc888816188e6f6f1c7c
auzmartist <auz.martin@gmail.com>
2025-01-22 22:42:00 -0500

    Add x-host Alpine directive support to alpine templating options

--- src/template/alpine/x-host.ts
+++ src/template/alpine/x-host.ts
@@ -1,38 +1,57 @@
-import { evaluateLater, type Alpine } from 'alpinejs'
+import Alpine from 'alpinejs'
 
 function getHost(el) {
   if (el.host) return el.host
-  if (el.parentNode) {
-    return getHost(el.parentNode)
-  } else {
-    return null
+  if (el.parentNode) return getHost(el.parentNode)
+  return null
+}
+
+/**
+ *
+ * @param data reactive data object
+ * @param el element with a property to add to reactive data
+ * @param key the property name to add
+ */
+function addToReactive<E extends HTMLElement>(data: ReturnType<typeof Alpine.reactive>, el: E, key: keyof E) {
+  const desc = Object.getOwnPropertyDescriptor(el.constructor.prototype, key)
+  if (desc && desc.get) {
+    // assign getter to reactive data
+    Object.defineProperty(el, key, {
+      get() {
+        const value = desc.get.call(this)
+        data[key] = value
+        return value
+      },
+      set: desc.set
+        ? function (value) {
+            data[key] = value
+            return desc.set.call(this, value)
+          }
+        : undefined,
+      configurable: true,
+      enumerable: true,
+    })
+    data[key] = el[key]
   }
 }
 
-function hostData(el) {
-  const data = {}
+function hostData(el, props: string[]) {
+  const data = Alpine.reactive({})
   for (const key in el) {
     if (key in HTMLElement.prototype || key === 'render') continue
-    data[key] = el[key]
+    if (props?.length != null && !props.includes(key)) continue
+    addToReactive(data, el, key)
   }
   return data
 }
 
-export function xHost(alpine: Alpine) {
-  return function (el, { value, modifiers, expression }, { evaluateLater }) {
+export function xHost(alpine: typeof Alpine) {
+  return function (el, { value, modifiers, expression }, { evaluate }) {
     const host = getHost(el)
     if (host.constructor.name !== 'HybridsElement') {
       throw new Error('x-host directive must only be used inside a HybridsElement')
     }
-    const evaluate = evaluateLater(expression)
-
-    evaluate((data) => {
-      if (typeof data === 'object') {
-        alpine.addScopeToNode(el, { ...data, ...hostData(host) })
-        console.log({ ...data, ...hostData(host) })
-      } else {
-        throw new Error('x-host expects an object from x-data')
-      }
-    }).before('data')
+    const props = expression ? evaluate(expression) : null
+    alpine.addScopeToNode(el, hostData(host, props))
   }
 }
--- src/test/test-alpine.ts
+++ src/test/test-alpine.ts
@@ -1,25 +1,45 @@
-import Alpine, { directive } from 'alpinejs'
-import { alpine, build, hy } from '../index.js'
+import Alpine from 'alpinejs'
+import { alpine, build } from '../index.js'
 
 alpine.config(Alpine)
 
 interface TestAlpine extends HTMLElement {
   disabled: boolean
+  count: number
+  double: number
 }
 
 const TestAlpine = build<TestAlpine>(({ shadow, html, reflect }) => ({
   tag: 'test-alpine',
   disabled: reflect(false),
+  count: {
+    value: 0,
+    connect: (host, key) => {
+      setInterval(() => ++host[key], 500)
+    },
+  },
+  double: ({ count }) => count * 2,
   render: shadow(
-    ({ disabled }) => html`
-      <div x-host="{ open: false }">
+    ({ disabled, double }) => html`
+      <!--
+        x-host is a custom directive used to mirror reactive data from Hybrids to Alpine scoped data.
+        This allows Alpine to react to changes in the Hybrids host data.
+        Re-renders are still triggered by Hybrids' reactivity system.
+        If your comonent is not re-rendering, check that the re-render is being triggered.
+
+        Use x-host without an attribute value ("x-host") to bind all non-native HTMLElement properties to the Alpine scope.
+      -->
+      <div x-host="['disabled', 'double']" x-data="{ open: false }">
         <div x-show="!disabled" :class="open ? '' : 'hidden'">
+          <!-- 'open' acts like a "private" variable -->
           <button @click="open = !open">Toggle Open</button>
           <div x-show="open">Open</div>
           <div x-show="!open">Closed</div>
         </div>
-        <span x-show="disabled">Disabled</span>
-        <button onclick="${toggleDisabled}">${disabled ? 'Enable' : 'Disable'}</button>
+        <!-- reactive data sharing between Hybrids and Alpine -->
+        <span x-show="disabled"><b>Disabled</b></span>
+        <!-- Hybrids onX handler to update Hybrids data directly -->
+        <button onclick="${toggleDisabled}" x-text="disabled ? 'Enable: ' + double : 'Disable: ' + double"></button>
       </div>
     `
   ),
